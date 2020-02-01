de5d5f4e3e7c4b0d57a0a768e0a52ab518b6c1b4
KRYSTALS-PC\krystal <krystalmc478@gmail.com>
2020-02-01 21:53:29 +0800

    added hit animation

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -8,6 +8,8 @@ public class CameraManager : MonoBehaviour
     public Transform cubeCentre;
     public Vector3 startPos;
     Player player;
+    public AnimationCurve shake;
+    public bool isShaking;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,14 @@ public class CameraManager : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, startPos + new Vector3(player.GetAxis("RotateCameraHori") * 30f, player.GetAxis("RotateCameraVert") * 30f, 0),Time.deltaTime);
         transform.LookAt(cubeCentre);
 
+        if(isShaking)
+        {
+            screenShake();
+        }
+    }
+
+    void screenShake()
+    {
 
     }
 }
