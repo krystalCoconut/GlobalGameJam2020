648532d7a4cac7ff1a011113aad01e469c8a2290
Colton-O <33445050+Colton-O@users.noreply.github.com>
2020-02-02 11:36:19 +0800

    spriteRotTest

---
+++ Assets/SpriteTest/RotTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotTester : MonoBehaviour
+{
+    public Vector3 theRot;
+    public Sprite[] thesprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRot = transform.parent.transform.parent.eulerAngles;
+        if (theRot.x < 90 && theRot.x > -90)
+        {
+            GetComponent<SpriteRenderer>().sprite = thesprites[2];
+        }
+    }
+}
