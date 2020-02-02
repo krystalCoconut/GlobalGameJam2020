a2a80ad977068d9cec333481e509e340a36f564e
Colton-O <33445050+Colton-O@users.noreply.github.com>
2020-02-02 14:12:36 +0800

    breadHider

---
+++ Assets/Bread_Hider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bread_Hider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] BT = GameObject.FindGameObjectsWithTag("BT");
+        transform.parent = BT[Random.Range( 0, BT.Length)].transform;
+        transform.localPosition = Vector3.zero;
+        
+    }
+
+}
